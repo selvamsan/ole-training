8257fb26a4072c961887c1320876a49c4683bfba
Selvam Shanmugam <selvams@impc1548.htcindia.com>
2014-09-11 12:15:35 +0530

    Answers

--- google_ole.rb
+++ google_ole.rb
@@ -9,11 +9,11 @@ browser.goto 'http://www.google.com'
 # 
 # In an open browser, go to Google and right-click on the
 # search box, then click 'Inspect Element' to find the ID.
-search_field = browser.text_field(:id => "")
+search_field = browser.text_field(:id => "gbqfq")
 search_field.wait_until_present
 
 # Set the search_field to your search query.
-search_field.set ""
+search_field.set "selvam"
 
 sleep 30
 browser.quit
